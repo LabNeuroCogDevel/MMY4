e5061f6b460a000ca04647121bb91ccf5f580bef
Will Foran <willforan@gmail.com>
2015-02-17 15:33:00 -0500

    use correct only, provide tstats

--- model/Multimodalbehaveyear4.R
+++ model/Multimodalbehaveyear4.R
@@ -15,26 +15,38 @@ pdat <- pilotdata %>%
         mutate( sinceSwitch = trial - na.locf(na.rm=F,ifelse(is.switch,1,NA) * trial) +1) %>%
         mutate(invswitch = 1/sinceSwitch ) %>%
         # discard first trials before any switch and any missed trial
-        filter(!is.na(sinceSwitch) & sinceSwitch>0 & is.finite(seqRT))  #& response.type=='correct') 
-        
+        filter(!is.na(sinceSwitch) & sinceSwitch>0 & is.finite(seqRT) ) #& response.type=='correct') 
+
+pdat.correct <- pdat %>% filter(response.type=='correct')
+
 # exp dist mini block lengths, too few with 5 or more since switch
-p5 <- pdat %>% filter(sinceSwitch<5 )
+p5 <- pdat.correct %>% filter(sinceSwitch<5 )
 
 
 
-m.inv <- lmer(seqRT~ invswitch + (subj|invswitch), data=pdat)
-m.lin <- lmer(seqRT~ sinceSwitch + (subj|invswitch), data=pdat)
+m.inv <- lmer(seqRT~ invswitch   + (subj|invswitch), data=pdat.correct)
+m.lin <- lmer(seqRT~ sinceSwitch + (subj|invswitch), data=pdat.correct)
 
-m5.inv <- lmer(seqRT~ invswitch + (subj|invswitch), data=p5)
-m5.lin <- lmer(seqRT~ sinceSwitch + (subj|invswitch), data=p5)
+
+
+# look at response type (correct, wrong) # also have noresp, but no RT for that
+# fails to converge :(
+#   cpc.inv <- glmer(response.type ~ invswitch + (subj|invswitch),     family='binomial', data=pdat)
+#   cpc.lin <- glmer(response.type ~ sinceSwitch + (subj|sinceSwitch), family='binomial', data=pdat)
+
+# t for each model (3rd coef in summary)
+tstat<-lapply(list(m.inv,m.lin,m5.inv,m5.lin),  function(x) summary(x)$coefficients[2,3] )
+names(tstat) <- c('m.inv','m.lin','m5.inv','m5.lin')
+print(unlist(tstat))
+
 
 print(AIC(m.inv,m.lin,m5.lin,m5.inv) )
 
-print(car::Anova(m))
+print(car::Anova(m.inv))
 
-gp <- ggplot(pdat,aes(x=sinceSwitch,y=seqRT)) +
-      geom_boxplot(aes(group=sinceSwitch))  +
-      geom_jitter(aes(color=subj),alpha=.4) +
+gp <- ggplot(p5,aes(x=sinceSwitch,y=seqRT,fill=trial.type)) +
+      geom_boxplot(aes(group=paste(trial.type,sinceSwitch)))  +
+      geom_point(aes(shape=as.factor(is_probe)),alpha=.4) +
       facet_wrap(~subj) +
       theme_bw()
 print(gp)
