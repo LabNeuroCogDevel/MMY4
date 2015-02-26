ce890f6e698a746de2bf9591e2f8d96a90066ecf
Will Foran <willforan@gmail.com>
2015-02-26 16:08:15 -0500

    update model to plot accuracy

--- model/Multimodalbehaveyear4.R
+++ model/Multimodalbehaveyear4.R
@@ -17,6 +17,11 @@ pdat <- pilotdata %>%
         # discard first trials before any switch and any missed trial
         filter(!is.na(sinceSwitch) & sinceSwitch>0 & is.finite(seqRT) ) #& response.type=='correct') 
 
+
+# plot accuracy
+acc <- pdat %>% group_by(subj,block,tt) %>% summarise( acc=sum(seqCrct==1)/n() ) #%>% ggplot(aes(x=tt,y=acc,color=subj)) + geom <- point()
+p<-ggplot(acc,aes(x=tt,y=acc,color=subj)) #+ geom_bar()
+
 pdat.correct <- pdat %>% filter(response.type=='correct')
 
 # exp dist mini block lengths, too few with 5 or more since switch
@@ -44,9 +49,9 @@ print(AIC(m.inv,m.lin,m5.lin,m5.inv) )
 
 print(car::Anova(m.inv))
 
-gp <- ggplot(p5,aes(x=sinceSwitch,y=seqRT,fill=trial.type)) +
+gp <- ggplot(p5%>%filter(is.na(is_probe) || is_probe==1),aes(x=sinceSwitch,y=seqRT,fill=trial.type)) +
       geom_boxplot(aes(group=paste(trial.type,sinceSwitch)))  +
-      geom_point(aes(shape=as.factor(is_probe)),alpha=.4) +
+       #geom_point(aes(shape=as.factor(is_probe)),alpha=.4) +
       facet_wrap(~subj) +
       theme_bw()
 print(gp)
